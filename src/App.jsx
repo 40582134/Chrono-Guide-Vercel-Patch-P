6dc0fa9b557417d8bcb64ff18316a0fe5b9c63bb
40582134 <124286253+40582134@users.noreply.github.com>
2024-04-29 04:14:39 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -35,7 +35,7 @@ const App = () => {
   }, []);
 
   return (
-    <Router>
+    <Router basename="/Chrono-Guide-Vercel-Patch-P">
       <div className="flex flex-col h-screen">
         <TopNavigation toggleSidebar={toggleSidebar} />
         <hr className="horizontal-divider" />
